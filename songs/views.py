fbf2ed4947d3c0437964fc0e854a7daac9515e36
Victoria Probst <victoria.probst13@gmail.com>
2022-06-17 17:04:58 -0400

    Created new View Function for getting Songs by ID and added new URLs path for Song Details

--- songs/views.py
+++ songs/views.py
@@ -23,3 +23,14 @@ def songs_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def song_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk) 
+        serializer = SongSerializer(song);
+        return Response(serializer.data)
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
+   
