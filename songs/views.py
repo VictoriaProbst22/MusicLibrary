10c29adb0fdfa70f75081d6f6df9b993bcb71778
Victoria Probst <victoria.probst13@gmail.com>
2022-06-17 17:23:45 -0400

    Created Update and Delete Functionality  and added Postman Collection

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status   
@@ -25,12 +26,19 @@ def songs_list(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_detail(request, pk):
-    try:
-        song = Song.objects.get(pk=pk) 
+    song = get_object_or_404(Song, pk=pk) 
+    if request.method == 'GET':
         serializer = SongSerializer(song);
         return Response(serializer.data)
-    except Song.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
+    elif request.method == 'PUT':
+        song = get_object_or_404(Song, pk=pk)
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
    
