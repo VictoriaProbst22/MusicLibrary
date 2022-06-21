abf7e19d9cc736ed274182f868a16bea6fe45863
VictoriaProbst22 <victoria.probst13@gmail.com>
2022-06-21 16:47:02 -0400

    Established map functionality to display Song Title, Artist and Album through React

---
+++ src/Components/DisplayMusic.jsx
@@ -0,0 +1,39 @@
+
+
+
+const DisplayMusic = (props) => {
+    console.log(props.songs);
+
+    return ( 
+        <table>
+            <thead>
+            <tr> <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Genre</th>
+                <th>Release Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {props.parentSongs.map((song, index)=> {
+              return (
+                <tr>
+                    <td>{index + 1}</td>
+                    <td>{song.title}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.album}</td>
+                </tr>
+              );  
+            })}
+            </tbody>
+    </table>
+    );
+}
+ 
+export default DisplayMusic;
+
+/** THIS PAGE IS ONLY USED WHEN YOU CAN CONFIRM YOUR GETTING DATA BACK
+ * THEN YOU WILL CREATE AN HTML TABLE AND MAP OVER THE SONGS PASSED DOWN THROUGH PROPS
+ * AND GENERATE THE TABLE DATA ROWS
+ */
+
